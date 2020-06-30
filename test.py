f8c0fe22878c58191c804c34739f49df60fbad86
Zayt02 <zaytpotter@gmail.com>
2020-06-30 12:25:48 +0700

    few changes

--- test.py
+++ test.py
@@ -1,2 +1,20 @@
-a = 'asdasd\n'
-print(a.replace('\n', ''))
+import pickle
+import os
+import sys
+
+size = int(sys.argv[1])
+dir = os.listdir('/mnt/disk1/tan_hm/corpus/train')
+files = [os.path.join(dir, i) for i in os.listdir(dir)]
+
+counter = 0
+seg = 2**30
+res = []
+
+while size > 0:
+    with open(files[counter]) as f:
+        res.append(f.read(seg))
+    counter += 1
+    size -= 1
+
+with open('/mnt/disk1/tan_hm/test_pkl.pkl', 'wb') as f:
+    pickle.dump(res, f, protocol=pickle.HIGHEST_PROTOCOL)
